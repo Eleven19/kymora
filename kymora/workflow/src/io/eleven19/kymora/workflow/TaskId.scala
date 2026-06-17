355170400fe2e02f2d1b2714cc4ae53d834d6b00
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2026-06-16 21:38:49 -0500

    feat(workflow): TaskId opaque type with runtime parser

---
+++ kymora/workflow/src/io/eleven19/kymora/workflow/TaskId.scala
@@ -0,0 +1,17 @@
+package io.eleven19.kymora.workflow
+
+import io.eleven19.kymora.workflow.internal.Validation
+import kyo.*
+
+opaque type TaskId = String
+
+object TaskId:
+  def parse(s: String): Result[Validation.Reason, TaskId] = Validation.check(s)
+  def unsafe(s: String): TaskId                           = s
+
+  extension (id: TaskId)
+    def value: String          = id
+    def segments: List[String] = id.split('.').toList.filter(_.nonEmpty)
+
+  given CanEqual[TaskId, TaskId] = CanEqual.derived
+end TaskId
---
+++ kymora/workflow/test/src/io/eleven19/kymora/workflow/TaskIdTests.scala
@@ -0,0 +1,24 @@
+package io.eleven19.kymora.workflow
+
+import io.eleven19.kymora.workflow.internal.Validation
+import kyo.*
+import kyo.test.*
+
+class TaskIdTests extends Test[Any]:
+  "TaskId.parse accepts valid id" in {
+    val r = TaskId.parse("kymora.vfs.jvm.compile")
+    assert(r.isSuccess && r.toMaybe.contains(TaskId.unsafe("kymora.vfs.jvm.compile")))
+  }
+  "TaskId.parse rejects slash" in {
+    val r = TaskId.parse("foo/bar")
+    assert(r == Result.fail(Validation.Reason.ContainsSlash))
+  }
+  "TaskId.value round-trips" in {
+    val id = TaskId.unsafe("foo.bar")
+    assert(id.value == "foo.bar")
+  }
+  "TaskId.segments splits on dot" in {
+    val id = TaskId.unsafe("foo.bar.baz")
+    assert(id.segments == List("foo", "bar", "baz"))
+  }
+end TaskIdTests
